96fe22f6b513f18668e668384d88a4bb63729fb7
João Jatobá <brandaojatoba@gmail.com>
2022-02-18 02:11:24 -0300

    Instanciei outro triangulo e comparo os dois.

--- Application.java
+++ Application.java
@@ -6,12 +6,26 @@ public class Application {
                 
                 System.out.println("Entre os lados do 1º triangulo:");
                 Triangle x = new Triangle();
+                Triangle y = new Triangle();
                 Scanner sc = new Scanner(System.in);
                 x.ladoA = sc.nextDouble();
                 x.ladoB = sc.nextDouble();
                 x.ladoC = sc.nextDouble();
-                double area = x.area();
-                System.out.println("A área do Triangulo é de: "+ area);
+                System.out.println("Entre os lados do 2º Triangulo:");
+                y.ladoA = sc.nextDouble();
+                y.ladoB = sc.nextDouble();
+                y.ladoC = sc.nextDouble();
+                double areaX = x.area();
+                double areaY = y.area();
+                System.out.println("A área do 1º Triangulo é de: "+ areaX);
+                System.out.println("A área do 2º Triangulo é de: "+ areaY);
+                if (areaX>areaY){
+                        System.out.println("O 1º Triangulo é maior.");
+                } else if (areaX<areaY){
+                        System.out.println("A área do 2º Triangulo é maior");
+                } else if (areaX==areaY){
+                        System.out.println("Os triangulos tem a mesma área.");
+                }
                 sc.close();
         }
 
